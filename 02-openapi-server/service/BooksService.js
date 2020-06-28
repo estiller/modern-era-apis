086c8d7b8944c133f7f5aec26fb44399ad39b170
Eran Stiller <git@stiller.blog>
2020-06-28 10:30:41 +0300

    Add launch settings for the server of demo 2

--- 02-openapi-server/service/BooksService.js
+++ 02-openapi-server/service/BooksService.js
@@ -1,139 +1,144 @@
-'use strict';
-
+"use strict";
 
 /**
  *
- * bookId Integer 
+ * bookId Integer
  * no response value expected for this operation
  **/
-exports.deleteBook = function(bookId) {
-  return new Promise(function(resolve, reject) {
+exports.deleteBook = function (bookId) {
+  return new Promise(function (resolve, reject) {
     resolve();
   });
-}
-
+};
 
 /**
  *
- * bookId Integer 
+ * bookId Integer
  * no response value expected for this operation
  **/
-exports.deleteBookBorrower = function(bookId) {
-  return new Promise(function(resolve, reject) {
+exports.deleteBookBorrower = function (bookId) {
+  return new Promise(function (resolve, reject) {
     resolve();
   });
-}
-
+};
 
 /**
  *
- * bookId Integer 
+ * bookId Integer
  * returns Book
  **/
-exports.getBook = function(bookId) {
-  return new Promise(function(resolve, reject) {
+exports.getBook = function (bookId) {
+  return new Promise(function (resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "name" : "name",
-  "borrower" : "",
-  "id" : 0
-};
+    examples["application/json"] = {
+      name: "name",
+      borrower: "",
+      id: 0,
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
 
 /**
  *
- * bookId Integer 
+ * bookId Integer
  * returns User
  **/
-exports.getBookBorrower = function(bookId) {
-  return new Promise(function(resolve, reject) {
+exports.getBookBorrower = function (bookId) {
+  return new Promise(function (resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "name" : "name",
-  "id" : 0
-};
+    examples["application/json"] = {
+      name: "name",
+      id: 0,
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
 
 /**
  *
  * returns List
  **/
-exports.getBooks = function() {
-  return new Promise(function(resolve, reject) {
+exports.getBooks = function () {
+  return new Promise(function (resolve, reject) {
     var examples = {};
-    examples['application/json'] = [ {
-  "name" : "name",
-  "borrower" : "",
-  "id" : 0
-}, {
-  "name" : "name",
-  "borrower" : "",
-  "id" : 0
-} ];
+    examples["application/json"] = [
+      {
+        name: "The Hitchhiker's Guide to the Galaxy",
+        id: 1,
+      },
+      {
+        name: "The Restaurant at the End of the Universe",
+        id: 2,
+      },
+      {
+        name: "Life, the Universe and Everything",
+        id: 3,
+      },
+      {
+        name: "So Long, and Thanks for All the Fish",
+        id: 4,
+      },
+      {
+        name: "Mostly Harmless",
+        id: 5,
+      },
+    ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
 
 /**
  *
- * body Book 
- * bookId Integer 
+ * body Book
+ * bookId Integer
  * returns Book
  **/
-exports.putBook = function(body,bookId) {
-  return new Promise(function(resolve, reject) {
+exports.putBook = function (body, bookId) {
+  return new Promise(function (resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "name" : "name",
-  "borrower" : "",
-  "id" : 0
-};
+    examples["application/json"] = {
+      name: "name",
+      borrower: "",
+      id: 0,
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
 
 /**
  *
- * body User 
- * bookId Integer 
+ * body User
+ * bookId Integer
  * returns User
  **/
-exports.putBookBorrower = function(body,bookId) {
-  return new Promise(function(resolve, reject) {
+exports.putBookBorrower = function (body, bookId) {
+  return new Promise(function (resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "name" : "name",
-  "id" : 0
-};
+    examples["application/json"] = {
+      name: "name",
+      id: 0,
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
